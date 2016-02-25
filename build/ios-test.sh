fae3ad231bc84270e1aeddcd6ac2692cc5bf3011
matthewpalmer <matt@matthewpalmer.net>
2016-02-25 17:53:32 +1100

    Try to fix travis builds

--- build/ios-test.sh
+++ build/ios-test.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-echo $PWD; cd iOS/Charter && xctool -sdk iphonesimulator -workspace Charter.xcworkspace -scheme "Charter" test && cd ..;
+echo $PWD; cd iOS/Charter && xctool -sdk iphonesimulator -workspace Charter.xcworkspace -scheme "Charter" -destination "platform=iOS Simulator,name=iPhone 6,OS=9.2" test && cd ..;
