8bc2850139b76d4c4893608890d8fdb43c3ff962
matthewpalmer <matt@matthewpalmer.net>
2016-03-04 19:20:47 +1100

    Fix tests for inconsistent dict ordering

--- iOS/Charter/Charter/Services/Data Sources/Networking/EmailThreadNetworkDataSourceImplTest.swift
+++ iOS/Charter/Charter/Services/Data Sources/Networking/EmailThreadNetworkDataSourceImplTest.swift
@@ -26,7 +26,12 @@ class EmailThreadNetworkDataSourceImplTest: XCTestCase {
         
         mockSession.assertionBlockForRequest = { request in
             XCTAssertEqual(request.allHTTPHeaderFields!["Authorization"], "Basic Y2xpZW50OmFHLWNpckYtT2ctZlUt")
-            XCTAssertEqual(request.URL?.absoluteString, "http://charter.ws:8080/charter/emails?sort_by=-date&page=1&pagesize=25&filter=%7BinReplyTo:null%7D")
+            
+            // We can't guarantee the ordering of the query parameters.
+            let expected = "http://charter.ws:8080/charter/emails?sort_by=-date&page=1&pagesize=25&filter=%7BinReplyTo:null%7D".characters.sort()
+            let actual = request.URL!.absoluteString.characters.sort()
+            
+            XCTAssertEqual(expected, actual)
             correctRequest.fulfill()
         }
         
@@ -55,8 +60,9 @@ class EmailThreadNetworkDataSourceImplTest: XCTestCase {
         let mockSession = NetworkingSessionMock(dataTask: dataTask)
         mockSession.assertionBlockForRequest = { request in
             // This is an important assertion: make sure the URL gets properly (but not necessarily spec compliant-ly...) URL encoded for RESTHeart
-            let expectedURL = "http://charter.ws:8080/charter/emails?pagesize=1000&filter=%7B_id:%7B$in:%5B'CA%2BY5xYfqKR6yC2Q-G7D9N7FeY%3Dxs1x3frq%3D%3DsyGoqYpOcL9yrw@mail.gmail.com','CAA%2BbWKUPaPtN8sFiNzp%2BxrZV47iJdi9g_3hUrAiaG39-j7YPpg@mail.gmail.com','CAA%2BbWKXSTkAuFC%2BNTLPaO0XAOa09kUt5CNNNrGyoL%2BSZgf7ZhQ@mail.gmail.com','CACR_FB63_19%2B4uwtyUgnt7MQ6KY7NPCTS1p1K7r8Xw3AXRBmNw@mail.gmail.com','314A26E1-C235-4C68-81EE-18B2284CEC5A@lorentey.hu','CA%2BY5xYcEiEvW8fT55UGT7gUEWUzW3e-cgf6UgbGjdJd7B1Dd1A@mail.gmail.com','CA%2BY5xYcgdxbqo7cHR4KXdBXu6stncn4WVhBea9GroeFLJLy6gw@mail.gmail.com','DE386A8B-8454-4137-AB74-E29BD13C621C@architechies.com','CA%2BY5xYcNqZTOCh5LZnM%3DY07_o8ShL-e_xe9%2BitHqMkgtR-dNtw@mail.gmail.com'%5D%7D%7D&page=1"
-            XCTAssertEqual(request.URL!.absoluteString, expectedURL)
+            // We can't guarantee the ordering of the query parameters.
+            let expectedURL = "http://charter.ws:8080/charter/emails?pagesize=1000&filter=%7B_id:%7B$in:%5B'CA%2BY5xYfqKR6yC2Q-G7D9N7FeY%3Dxs1x3frq%3D%3DsyGoqYpOcL9yrw@mail.gmail.com','CAA%2BbWKUPaPtN8sFiNzp%2BxrZV47iJdi9g_3hUrAiaG39-j7YPpg@mail.gmail.com','CAA%2BbWKXSTkAuFC%2BNTLPaO0XAOa09kUt5CNNNrGyoL%2BSZgf7ZhQ@mail.gmail.com','CACR_FB63_19%2B4uwtyUgnt7MQ6KY7NPCTS1p1K7r8Xw3AXRBmNw@mail.gmail.com','314A26E1-C235-4C68-81EE-18B2284CEC5A@lorentey.hu','CA%2BY5xYcEiEvW8fT55UGT7gUEWUzW3e-cgf6UgbGjdJd7B1Dd1A@mail.gmail.com','CA%2BY5xYcgdxbqo7cHR4KXdBXu6stncn4WVhBea9GroeFLJLy6gw@mail.gmail.com','DE386A8B-8454-4137-AB74-E29BD13C621C@architechies.com','CA%2BY5xYcNqZTOCh5LZnM%3DY07_o8ShL-e_xe9%2BitHqMkgtR-dNtw@mail.gmail.com'%5D%7D%7D&page=1".characters.sort()
+            XCTAssertEqual(request.URL!.absoluteString.characters.sort(), expectedURL)
             correctRequest.fulfill()
         }
         
